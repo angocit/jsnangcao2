697c00c3e9eba682ded1dc732f47c875ec5b77c1
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-01-23 18:12:05 +0700

    upload

--- frontend/main.js
+++ frontend/main.js
@@ -7,10 +7,53 @@
 // document.querySelectorAll
 // const content = document.querySelector('.content');
 // console.dir(content);
-// Mục tiêu muốn tahy 3.1 = .3111
-const table = document.querySelectorAll('table');
-// console.dir(table[1].firstElementChild.children[0].children[2]);
-const td = table[1].firstElementChild.children[0].children[2];
-console.dir(td);
-// td.innerHTML = '3.111';
+// // Mục tiêu muốn tahy 3.1 = .3111
+// const table = document.querySelectorAll('table');
+// // console.dir(table[1].firstElementChild.children[0].children[2]);
+// const td = table[1].firstElementChild.children[0].children[2];
+// console.dir(td);
+// // td.innerHTML = '3.111';
+// Promise
+// Callback Hell
 
+// setTimeout(()=>{
+//     console.log(`soos 1`);
+//     setTimeout(()=>{
+//         console.log(`soos 2`);
+//         setTimeout(()=>{
+//             console.log(`soos 3`);
+//             setTimeout(()=>{
+//                 console.log(`soos 4`);
+//             },100);
+//         },500);
+//     },1000);
+// },2000)
+
+// const data = new Promise((resolve, reject) => {
+//     reject("Lỗi");
+// });
+// console.log(data);
+// // data.then((kq) => {
+// //     console.log(kq);
+// // });
+// data.catch((err) => {
+//     console.log(err);
+// });
+
+// Async/Await
+
+// const data = fetch('http://localhost:8080/products');
+// data.then((rs)=>{
+//     // console.log(rs.json());
+//     return rs.json();
+// }).then((rs) => {
+//     console.log(rs);
+// });
+// console.log(data);
+
+const getData = async()=>{
+    const data = await fetch('http://localhost:8080/products');
+    const product = await data.json();
+    console.log(product);
+}
+getData();
